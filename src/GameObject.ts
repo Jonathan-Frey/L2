20978807a6cdfde4d01587b7b28fb57c98bcc23f
jf223rf <jf223rf@student.lnu.se>
2024-09-12 23:27:53 +0200

    add abstact GameObject class

--- src/GameObject.ts
+++ src/GameObject.ts
@@ -1,1 +1,13 @@
-export class GameObject {}
+export abstract class GameObject {
+  // called each frame by the GameEngine Object
+  update(delta: number) {
+    this.#process(delta);
+    this.#render(delta);
+  }
+
+  // For behavior and interaction.
+  #process(delta: number) {}
+
+  // For rendering the object to the screen.
+  #render(delta: number) {}
+}
