8169ac009809646d18ac46b125a8e007231e9a29
jf223rf <jf223rf@student.lnu.se>
2024-09-12 23:52:27 +0200

    change overwriteable methods from private to protected

--- src/GameObject.ts
+++ src/GameObject.ts
@@ -2,13 +2,13 @@ export default abstract class GameObject {
   // called each frame by the GameEngine Object
   update(delta: number) {
     console.log("hello from game object");
-    this.#process(delta);
-    this.#render(delta);
+    this.process(delta);
+    this.render(delta);
   }
 
   // For behavior and interaction.
-  #process(delta: number) {}
+  protected process(delta: number) {}
 
   // For rendering the object to the screen.
-  #render(delta: number) {}
+  protected render(delta: number) {}
 }
