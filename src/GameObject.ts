aa2ed1342758fdde16f7c493181da6bad3f4c219
Jonathan-Frey <jf223rf@student.lnu.se>
2024-09-13 14:03:46 +0200

    expand gameObject to reference parent and children

--- src/GameObject.ts
+++ src/GameObject.ts
@@ -1,9 +1,32 @@
 export default abstract class GameObject {
+  #parent: GameObject | null = null;
+  #children: GameObject[] = [];
+
   // called each frame by the GameEngine Object
   update(delta: number, ctx: CanvasRenderingContext2D
   ) {
+    this.#children.forEach((child) => {
+      child.update(delta, ctx);
+    });
     this.process(delta);
     this.render(delta, ctx);
+  }
+
+  protected getParent() {
+    return this.#parent;
+  }
+
+  protected setParent(parent: GameObject) {
+    this.#parent = parent;
+  }
+
+  protected addChild(child: GameObject) {
+    child.setParent(this);
+    this.#children.push(child);
+  }
+
+  protected removeChild(child: GameObject) {
+      this.#children.filter((c) => c !== child);
   }
 
   // For behavior and interaction.
